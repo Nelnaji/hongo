3cff93af3a5e2eb2c2b72dc7c94afcc533f9e7ff
unknown <nawfal.phoenix@gmail.com>
2022-06-16 16:52:25 +0200

    push final pour présentation, projet non complet

--- public/js/modules/dark-mode.js
+++ public/js/modules/dark-mode.js
@@ -4,10 +4,10 @@ export const changeTheme = () => {
   // about section selectors
   const aboutSection = document.querySelector("main");
   const mainTextAbout = document.querySelector(".about-main-text");
-  const heroSection = document.querySelector(".hero-container");
+  const header = document.querySelector("header");
   const latestStyleSection = document.querySelector(".latest-styles");
   const recentProductsSection = document.querySelector(".recent-products");
-  const testimonialsSection = document.querySelector(".testimonials");
+  const testimonialsSection = document.querySelector(".testimonials-container");
   const latestBlogSection = document.querySelector(".latest-blog");
   const contactSection = document.querySelector(".contact");
 
@@ -20,8 +20,13 @@ export const changeTheme = () => {
   const sectionOne = document.querySelector("header");
 
   btntheme.addEventListener("click", (e) => {
-    nav.classList.toggle("dark-nav");
-
+    // changement du hero
+    // nav.classList.toggle("dark-nav");
+    // header dark mode
+    header.classList.toggle("header-dark");
+    // testimonials+style dark mode
+    testimonialsSection.classList.toggle("dark-mode");
+    latestStyleSection.classList.toggle("dark-mode");
     //  changement de l'icone de la nav en dark mode.
     if (navLogo.getAttribute("src") === "./public/img/logo.png") {
       navLogo.src = "./public/img/white-logo.png";
@@ -45,6 +50,8 @@ export const changeTheme = () => {
       e.target.classList.remove("fa-sun");
       e.target.classList.add("fa-moon");
     }
+
+    // changing Hero theme
 
     aboutSection.classList.toggle("dark-mode-about");
     mainTextAbout.classList.toggle("dark-mode-main-text");
